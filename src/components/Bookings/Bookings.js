4e2493cee467b5244eb98b1f65fc4921b3afcd16
RianIslam <rianislamdr@gmail.com>
2021-03-27 02:23:17 +0600

    fetch the data and show the ui

--- src/components/Bookings/Bookings.js
+++ src/components/Bookings/Bookings.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Bookings = () => {
+
+    const {bookings,setBookings} = useState
+    useEffect(()=>{
+        fetch('http://localhost:5000/booking')
+        .then(res => res.json())
+        .then(data => setBookings(data))
+    },[])
     return (
         <div>
             
