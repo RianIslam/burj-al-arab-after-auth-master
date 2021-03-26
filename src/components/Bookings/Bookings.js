13232f1bc0ff5eae93d277098c799efc652e1ee8
RianIslam <rianislamdr@gmail.com>
2021-03-27 03:13:07 +0600

    bookings showing

--- src/components/Bookings/Bookings.js
+++ src/components/Bookings/Bookings.js
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from 'react'
 
 const Bookings = () => {
 
-    const {bookings,setBookings} = useState
-    useEffect(()=>{
+    const [bookings,setBookings] = useState([])
+
+
+    useEffect(() => {
         fetch('http://localhost:5000/booking')
         .then(res => res.json())
         .then(data => setBookings(data))
@@ -13,7 +15,7 @@ const Bookings = () => {
             <h3>You have : {bookings.length} bookings</h3>
 
             {
-                booking.Bookings(book => <li>{book.name} from : {book.checkIn} To:{book.checkOut}</li>)
+                bookings.map(book => <li>{book.name} from : {book.checkIn} To:{book.checkOut}</li>)
             }
         </div>
     )
